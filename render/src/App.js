89cd495dc51e826fa0c4d6ed4fdfcf9733e07537
neos55555 <778007921@qq.com>
2020-12-25 11:02:49 +0800

    add router

--- render/src/App.js
+++ render/src/App.js
@@ -4,17 +4,39 @@ import '@/assets/css/default.scss'
 // import 'braft-editor/dist/index.css'
 import './App.scss';
 import './media.scss';
-import { ipcRenderer } from 'electron'
-
+// import { ipcRenderer } from 'electron'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 export default class App extends Component {
 
 	render () {
 	  return (
-	  	<div>App
-				<button onClick={() => ipcRenderer.send('getList', 'list args')}>click</button>
-			</div>
+			<Router>
+				<div className="container main-content-wrapper">
+		    	<Switch>
+						<Route path="/123">
+							<div>这里是123</div>
+		        </Route>
+		        <Route path="/">
+							<div>App
+								<Link to="/123">goto 123</Link>
+								{/* <button onClick={() => ipcRenderer.send('getList', 'list args')}>click</button> */}
+							</div>
+		        </Route>
+		        
+		        <Route path="*">
+		          404
+		        </Route>
+		      </Switch>
+				</div>
+	    </Router>
+	  	
 	  );
 	}
 }
